e332ace05f4d1f0376a4ec336e32d723a6f8e0f4
MichaelOlea <44531215+MichaelOlea@users.noreply.github.com>
2021-02-13 16:16:26 -0800

    Delete sha256 example.py

--- sha256 example.py
+++
@@ -1,5 +0,0 @@
-import hashlib
-
-message = input("Enter the secret message: ")
-hash = hashlib.sha256(str(message).encode("utf-8")).hexdigest()
-print("The hash fucntion of the message *" + message + "* is " + hash)
